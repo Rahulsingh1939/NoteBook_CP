a190471e99da5cfe70a5a7a8d20fa1f6d73a8d76
Rahulsingh1939 <rahulsingh19390@gmail.com>
2025-01-03 19:10:18 +0530

    Added DSU by Rank, SIze

--- Graph/DSU.cpp
+++ Graph/DSU.cpp
@@ -38,3 +38,40 @@ public:
     }
 };
 
+class DSU_bySize
+{
+    vector<int> size, parent;
+
+public:
+    DSU_bySize(int n)
+    {
+        size.resize(n + 1, 1);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+    // Find Parent function with Path Compression
+    int findPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    void unionBySize(int u, int v)
+    {
+        int pU = findPar(u);
+        int pV = findPar(v);
+        if (pU == pV)
+            return;
+        if (size[pU] < size[pV])
+        {
+            parent[pU] = pV;
+            size[pV] += size[pU];
+        }
+        else
+        {
+            parent[pV] = pU;
+            size[pU] += size[pV];
+        }
+    }
+};
